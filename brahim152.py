4c0c3f0a501c490cdd1434b32b32f62724fbf331
sellingmysoulai <160229547+sellingmysoulai@users.noreply.github.com>
2024-04-13 13:28:00 +0200

    Update brahim152.py

--- brahim152.py
+++ brahim152.py
@@ -409,7 +409,7 @@ def transformMOS(erin, eruit, additionalHours, subtractHours, room_type, locView
             locView.update({uniqLoc: 1})
 
     total_intervals = len(intervals_df)
-    non_zero_intervals = intervals_df[df['people_counter_all'] > 0]
+    non_zero_intervals = intervals_df[intervals_df['people_counter_all'] > 0]
     num_non_zero_occupancies = len(non_zero_intervals)
     occupancy_percentage = (num_non_zero_occupancies / total_intervals) * 100
 
