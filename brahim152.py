2bcf74b5cf82789094fae9aa46d563b70494c921
sellingmysoulai <160229547+sellingmysoulai@users.noreply.github.com>
2024-04-13 12:49:06 +0200

    Update brahim152.py

--- brahim152.py
+++ brahim152.py
@@ -411,6 +411,7 @@ def price_to_float(price_str):
 
 def analyze_mos_file(mos_file):
     # Load data
+    st.write("sven,",mos_file)
     mos_file = mos_file.seek(0)
     df = pd.read_csv(mos_file)
     df['received_at'] = pd.to_datetime(df['received_at'])
